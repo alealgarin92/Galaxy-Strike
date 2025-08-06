a1316f4b839240a2947aabd318d832e09ea4170d
Alejandro Algarin <alejandro.algarin@hotmail.com>
2025-08-06 20:06:17 -0300

    Screen To WorldPoint Target

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -5,13 +6,18 @@ using UnityEngine.UI;
 public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
 
     bool isFiring = false;
 
-
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
     private void Update()
     {
         ProcessFiring();
+        MoveCrosshair();
     }
     public void OnFire(InputValue value)
     {
@@ -26,5 +32,10 @@ public class PlayerWeapon : MonoBehaviour
             emissionModule.enabled = isFiring;
         }
         
+    }
+
+    void MoveCrosshair()
+    {
+        crosshair.position = Input.mousePosition;
     }
 }
