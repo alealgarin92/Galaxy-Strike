c4edd549ea04efc00fcffaef81b1ca7c039e5b64
ale4287 <alejandro.algarin@hotmail.com>
2025-08-12 04:29:50 -0300

    Nested Prefaps

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject destroyedVFX;
+    private void OnParticleCollision(GameObject other)
+    {
+        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
